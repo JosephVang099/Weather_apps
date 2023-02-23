c9e0524fd2a143af87175316db8082495483fc5c
JosephVang099 <119532720+JosephVang099@users.noreply.github.com>
2023-02-23 08:55:25 -0500

    Changes

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=e842d986ef654ba26e52320004570bc6&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
